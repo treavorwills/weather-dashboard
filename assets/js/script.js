90014255aecfa0fce48931b04567631c955952ce
twills <treav.wills@gmail.com>
2022-07-30 16:04:04 -0600

    add buttons for cities searched in the past

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@ var APIKey = "23744b8a06001f89b9ea14963a070d6c";
 var citySearch = $('#city-search');
 var searchHistory = JSON.parse(window.localStorage.getItem('localHistory')) || [];
 
+
+// display cities saved in local storage
+if(searchHistory !== ""){
+    displayPastCities();
+}
+
 $('#city-search').on('click', function (event) {
     var city = $('#city-input');
     event.preventDefault();
@@ -9,17 +15,23 @@ $('#city-search').on('click', function (event) {
 
     // save city to local storage
     saveCity(city);
-
-    displayPastCities();
 })
 
 
 function displayPastCities() {
     // access list from local storage
     var searchHistory = JSON.parse(window.localStorage.getItem('localHistory')) || [];
+    // set variable for the history-section
+    var historySection = $('#history-section');
     // append to history section
     searchHistory.forEach(function (item, index) {
-        console.log(item.cityName, index);        
+        // console.log(item.cityName, index);
+        var button = document.createElement('button');
+        button.setAttribute('type', 'button');
+        button.id = item.cityName;
+        button.className = 'btn btn-secondary m-1';
+        button.innerText = item.cityName;
+        historySection.append(button);
     });
 }
 
@@ -35,5 +47,13 @@ function saveCity(city) {
     // console.log(searchHistory);
     // save the updated search array of objects to local storage
     window.localStorage.setItem("localHistory", JSON.stringify(searchHistory));
+
+    var historySection = $('#history-section');
+    var button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.id = city.val();
+    button.className = 'btn btn-secondary m-1';
+    button.innerText = city.val();
+    historySection.append(button);
 }
 
